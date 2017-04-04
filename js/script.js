34fe24848476ac53da795d7cddb3111418388e08
Jedidiah Avelino <jed.avelino@gmail.com>
2017-04-05 07:18:00 +0900

    general updates

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 		$('[data-toggle="tooltip"]').tooltip();
 	}
 
+	function toggleChevron(e) {
+	    $(e.target)
+	        .prev('.panel-group-issuance .panel-heading')
+	        .find('span:nth-child(2) i')
+	        .toggleClass('glyphicon-minus glyphicon-plus');
+	}
+	$('#accordion').on('hidden.bs.collapse', toggleChevron);
+	$('#accordion').on('shown.bs.collapse', toggleChevron);
+
 });
