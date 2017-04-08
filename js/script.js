22f9d5ac4e4744ee38cc1f1249d5e34ed748fcb9
Jedidiah Avelino <jed.avelino@gmail.com>
2017-04-09 06:13:50 +0800

    added chart js and graphs module

--- js/script.js
+++ js/script.js
@@ -15,4 +15,36 @@ $(document).ready(function() {
 	$('#accordion').on('hidden.bs.collapse', toggleChevron);
 	$('#accordion').on('shown.bs.collapse', toggleChevron);
 
+
+
+	var pieChartElement = $('#pieChart');
+
+	var data = {
+		labels: [
+			"Red",
+			"Blue",
+			"Yellow"
+		],
+		datasets: [
+			{
+				data: [300, 50, 100],
+				backgroundColor: [
+					"#FF6384",
+					"#36A2EB",
+					"#FFCE56"
+				],
+				hoverBackgroundColor: [
+					"FF6384",
+					"#36A2EB",
+					"#FFCE56"
+				]
+			}]
+	};
+
+	var pieChart = new Chart(pieChartElement, {
+		type: 'doughnut',
+		data: data
+
+	});
+
 });
